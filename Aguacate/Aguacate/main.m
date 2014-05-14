6aaad9ed903f1d161d6c34c8b942f6311f7a8030
Mike Lai <m92lai@gmail.com>
2014-05-14 16:19:17 -0400

    project init

---
+++ Aguacate/Aguacate/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Aguacate
+//
+//  Created by Michael Lai on 2014-05-14.
+//  Copyright (c) 2014 Michael Lai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
