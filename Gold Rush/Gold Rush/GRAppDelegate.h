b7d5e306963e551a5991e681ebfd887b5cb7014c
Mike Lai <m92lai@gmail.com>
2014-05-14 16:03:16 -0400

    project init

---
+++ Gold Rush/Gold Rush/GRAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  GRAppDelegate.h
+//  Gold Rush
+//
+//  Created by Michael Lai on 2014-05-14.
+//  Copyright (c) 2014 Michael Lai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GRAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
